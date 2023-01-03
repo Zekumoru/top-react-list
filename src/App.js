f06897d57c6ab3cc279695b18c7502a5e3373181
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-03 16:31:19 +0100

    Add input interactivity

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import InputBar from './components/InputBar';
+import Overview from './components/Overview';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      tasks: [
+        { id: 0, task: 'Wash dishes' },
+        { id: 1, task: 'Hang clothes' },
+        { id: 2, task: 'Study bible' },
+        { id: 3, task: 'Take bath' },
+        { id: 4, task: 'Exercise for 10 minutes' },
+      ],
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleAddTaskClicked = this.handleAddTaskClicked.bind(this);
+  }
+
+  handleInputChange(value) {
+    this.setState({ input: value });
+  }
+
+  handleAddTaskClicked() {}
+
   render() {
     return (
-      <div>
-        <InputBar />
+      <div className="App">
+        <InputBar
+          value={this.state.input}
+          onInputChange={this.handleInputChange}
+          onAddTaskClicked={this.handleAddTaskClicked}
+        />
+        <Overview items={this.state.tasks} />
       </div>
     );
   }
--- src/components/InputBar.js
+++ src/components/InputBar.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 class InputBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleAddTaskClicked = this.handleAddTaskClicked.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.props.onInputChange(e.target.value);
+  }
+
+  handleAddTaskClicked() {}
+
   render() {
+    const { value } = this.props;
     return (
       <div className="InputBar">
-        <input type="text" placeholder="Write task..." />
+        <input
+          type="text"
+          placeholder="Write task..."
+          value={value}
+          onChange={this.handleInputChange}
+        />
         <button>Add task</button>
       </div>
     );
