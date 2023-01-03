dde8d26843c9d66557f7e5f887f6661a5f590419
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-03 23:03:24 +0100

    Style OverviewItem.js

--- src/components/OverviewItem.js
+++ src/components/OverviewItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './OverviewItem.scss';
 
 class OverviewItem extends React.Component {
   render() {
     const { number, item } = this.props;
     return (
       <li className="OverviewItem">
-        {number}. {item.task}
+        <span className="number">{number}.</span>
+        <span className="text">{item.task}</span>
+        <i className="fa-solid fa-trash" />
       </li>
     );
   }
